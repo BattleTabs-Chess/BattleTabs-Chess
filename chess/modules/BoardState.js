5d7a21f76ea728b23cba3938c985bec58e48b62e
Flyspeck101 <lawrence.chen8@education.nsw.gov.au>
2022-12-13 10:36:05 +1100

    Update BoardState.js

--- chess/modules/BoardState.js
+++ chess/modules/BoardState.js
@@ -1,1 +1,9 @@
+import * from "./ChessConstants.js";
 
+function BoardState(fen = ") {
+  let state = {};
+  let boardGrid = [];
+  for (let i = 0; i < BOARDHEIGHT; i++)
+}
+
+export ( BoardState() };
